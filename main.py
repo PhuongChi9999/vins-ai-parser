5ea2cf9d63a84c934ed4fdfa5db312c4456fd7a3
PhuongChi9999 <phuongchiphysics@gmail.com>
2026-02-24 09:40:36 +0100

    new main
    pour more pages recupéré

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ CSV_PATH = "vins.csv"
 
 def getsoup(url):
     try:
-        response = requests.get(url)
+        headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/117.0"
+        }
+        response = requests.get(url, headers= headers)
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
         print(f"Error fetching URL: {e}")
@@ -22,7 +25,8 @@ def getsoup(url):
 def parse_bordeaux():
     parsed_vins = 0
     current_page = 1
-    while parsed_vins < 90:
+    while current_page < 90: #while parsed_vins < 90:
+        print("Getting page: ", current_page)
         soup = getsoup(f"https://www.millesima.fr/bordeaux.html?page={current_page}")
         if soup is None:
             break
@@ -43,7 +47,7 @@ def parse_bordeaux():
                 if vin_soup is None:
                     continue
 
-                info = informations(vin_soup)
+                info = informations(vin_soup, False)
 
                 print(f"Parsed info: {info}")
 
@@ -51,7 +55,7 @@ def parse_bordeaux():
                 parsed_vins += 1
 
         current_page += 1
-
+        print("current_page: ", current_page)
 
 def main():
     init_csv(CSV_PATH)
