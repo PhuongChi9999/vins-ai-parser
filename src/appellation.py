3cf976090def646c1234fadf96f17f24200e7e13
PhuongChi9999 <phuongchiphysics@gmail.com>
2026-02-24 09:43:26 +0100

    Fix in appellation.py
    Supprimer les sauts de ligne inutiles

--- src/appellation.py
+++ src/appellation.py
@@ -9,5 +9,4 @@ def appellation(soup):
     data = json.loads(script.string)
     # Table is built by Next.js. We have no access to table data. So we need to parse it manually.
     return data["props"]["pageProps"]["initialReduxState"]["product"]["content"][
-        "attributes"
-    ]["appellation"]["value"]
+        "attributes"]["appellation"]["value"]
