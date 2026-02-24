94938985c28d3c4082347aa073cecd6cfea24b3e
PhuongChi9999 <phuongchiphysics@gmail.com>
2026-02-24 09:46:47 +0100

    Modify informations function to handle isparker flag
    Updated the 'informations' function to accept an 'isparker' parameter and conditionally use 'parker' or 'robert'.

--- src/informations.py
+++ src/informations.py
@@ -1,13 +1,17 @@
 from src.appellation import appellation
-from src.notation import parker, robinson, suckling
+from src.notation import robert, parker, robinson, suckling
 from src.prix import prix
 
 
-def informations(soup):
+def informations(soup, isparker):
     appellation_res = appellation(soup)
-    parker_res = parker(soup)
+    parkerrobert_res = None
+    if(isparker):
+        parkerrobert_res== parker(soup)
+    else:
+        parkerrobert_res = robert(soup)
     robinson_res = robinson(soup)
     suckling_res = suckling(soup)
     prix_res = prix(soup)
 
-    return f"{appellation_res},{parker_res},{robinson_res},{suckling_res},{prix_res}"
+    return f"{appellation_res},{parkerrobert_res},{robinson_res},{suckling_res},{prix_res}"
